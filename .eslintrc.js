554c685d1119b3d48aa08bd843a1907654f54ccb
jkassner <julianokassner@hotmail.com>
2020-05-24 21:11:58 -0300

    aceitar any

--- .eslintrc.js
+++ .eslintrc.js
@@ -79,7 +79,7 @@ module.exports = {
 		// TypeScript
 		quotes: ['warn', 'single'],
 		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/no-explicit-any': 2
+		'@typescript-eslint/no-explicit-any': 'off',
 
 		// allow debugger during development only
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
